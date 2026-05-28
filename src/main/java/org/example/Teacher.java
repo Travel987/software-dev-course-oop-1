d7a3e6092a8b82a4d6128a727cf67d69a4949e41
Travel987 <brianapowers78@gmail.com>
2026-05-28 13:53:57 -0500

    Finished OOP Practice 2

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -13,4 +13,32 @@ public class Teacher {
 
     // Add a toString method that returns the teacher's name and className in the following format:
     // name (className)
+    private String name;
+    private String className;
+
+    public Teacher(String name, String className) {
+        this.name = name;
+        this.className = className;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String toString() {
+        return name + " (" + className + ")";
+    }
 }
+
